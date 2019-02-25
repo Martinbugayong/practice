c38611d3a9c6de2aca2d63fe1916d76b35330ca6
martinbugayong <martinbugayong@gmail.com>
2019-02-24 18:57:52 -0800

    practice

---
+++ poke.js
@@ -0,0 +1,19 @@
+/*
+must be able to access  
+
+Must contain:
+object - done
+string - done
+number - done
+boolean
+array - done
+
+object in an object - done
+array in and array - done
+array in an object - done
+object in an array - done
+*/
+
+let paragraphs = document.body.getElementsByTagName("p");
+
+document.body.insertBefore(paragraphs[4], paragraphs[2]);
